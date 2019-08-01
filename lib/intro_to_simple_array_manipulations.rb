06ca8e16c238f428f84fee190bd7b43fc348dd88
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-01 17:50:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,14 @@ end
 def pop_with_args(array)
   array.pop(2)
 end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(arr1,arr2)
+  
